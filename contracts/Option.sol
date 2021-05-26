f50bac96d6a24f91516496bd92ec6a4d3004a7fd
Ferdous Bhai <ferdousbd@gmail.com>
2021-05-26 11:44:27 +0800

    Improved TO DO list at Option.sol

--- contracts/Option.sol
+++ contracts/Option.sol
@@ -1,4 +1,3 @@
-// contracts/GLDToken.sol
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
@@ -9,7 +8,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
  * @title An option contract.
  * @dev An ERC20 token that represents either a call or a put option contract of an asset with a specific strike and expiry.
  * The ERC20 contracts are initiated by the OptionFactory contract.
- * TO DO: Use DAI as base currency + charge a flat fee for issuing and exercising contracts. $10?
+ * TO DO: Use DAI as base currency + charge a flat fee for issuing contracts.
  * TO DO: Handle puts.
  */
 contract Option is ERC20 {
@@ -17,14 +16,16 @@ contract Option is ERC20 {
 
     OptionType optionType;
     IERC20 asset; // the underlying asset
-    uint256 time;
-    uint256 strike;
+    uint256 time; // expiry time
+    uint256 strike; // strike price in DAI
 
-    mapping(address => uint256) asset_balance;
-    uint256 total_asset_balance;
-    uint256 outstanding_asset_balance;
+    mapping(address => uint256) asset_balances; // This is expensive. Is there a better way?
+    uint256 total_asset_balance; // keeps track of amount of assets locked in to issue options
+    uint256 outstanding_asset_balance; // keeps track of amount of assets not exercised yet
+    uint256 fee; // fee per contract issuance in DAI
+    uint256 total_fees; // total fees collected in DAI
 
-    address daoAddress;
+    address daoAddress; // fees will be sent here after contract expiry
 
     constructor(
         OptionType _type,
@@ -51,28 +52,38 @@ contract Option is ERC20 {
     }
 
     function issue() external notExpired {
-        _mint(msg.sender, msg.value / 100); //TO DO: How to check that the caller is sending units of underlying asset?
-        asset_balance[msg.sender] += msg.value;
+        //TO DO: receive the underlying asset
+        _mint(msg.sender, msg.value / 100);
+        asset_balances[msg.sender] += msg.value;
         total_asset_balance += msg.value;
         outstanding_asset_balance += msg.value;
     }
 
     function exercise() external notExpired {
-        // TO DO: check that the uniswap price of asset in DAI is more than the strike price.
+        // TO DO: require that the uniswap price (TWAP) of asset in DAI is more than the strike price.
         // TO DO: receive strike price * unit of asset in DAI.
-        // TO DO: receive call contract tokens, 'c' + burn the tokens.
+        // TO DO: receive call contract tokens, 'c'
         uint256 a = c * 100; // 'c' is the number of call contract tokens. 'a' is the units of asset to send back.
         asset.transfer(msg.sender, a);
         outstanding_asset_balance -= a;
+        // TO DO: burn the call contract tokens receiveed, 'c'
     }
 
     function redeem() external expired {
-        uint256 v =
-            asset_balance[msg.sender] *
-                (outstanding_asset_balance / total_asset_balance);
-        asset.transfer(msg.sender, v);
-        total_asset_balance -= asset_balance[msg.sender];
-        outstanding_asset_balance -= asset_balance[msg.sender];
-        asset_balance[msg.sender] = 0;
+        asset.transfer(
+            msg.sender,
+            asset_balances[msg.sender] *
+                (outstanding_asset_balance / total_asset_balance)
+        );
+        total_asset_balance -= asset_balances[msg.sender];
+        outstanding_asset_balance -= asset_balances[msg.sender];
+        asset_balances[msg.sender] = 0;
+    }
+
+    function collectFees() external expired {
+        require(total_fees > 0, "Fees were already withdrawn!");
+        require(msg.sender == daoAddress);
+        DAI.transfer(daoAddress, total_fees); // TODO: Handle DAI
+        total_fees = 0;
     }
 }
